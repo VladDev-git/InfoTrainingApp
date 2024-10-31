b22794c9f51316fcfb68a7c5ac6a24433241c845
Vlad Petryk <valentinpetrik63@gmail.com>
2024-11-01 00:35:41 +0200

    alpha_1

--- app/src/main/java/com/example/infoapp/MainActivity.kt
+++ app/src/main/java/com/example/infoapp/MainActivity.kt
@@ -1,47 +1,50 @@
 package com.example.infoapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.example.infoapp.ui.theme.InfoAppTheme
+import com.example.infoapp.ui_components.MainTopBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+            val topBarTitle by remember {
+                mutableStateOf("Training center")
+            }
+            //val coroutineScope = rememberCoroutineScope()
+
             InfoAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                ModalNavigationDrawer(
+                    drawerState = drawerState,
+                    drawerContent = {
+
+                    }
+                ) {
+                    Scaffold(
+                        topBar = {
+                            MainTopBar(title = topBarTitle, drawerState)
+                        }
+                    ) {
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InfoAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/infoapp/ui_components/MainTopBar.kt
@@ -0,0 +1,36 @@
+package com.example.infoapp.ui_components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainTopBar(title: String, drawerState: DrawerState) {
+    val coroutineScope = rememberCoroutineScope()
+
+    TopAppBar(
+        title = {
+            Text(text = title)
+        },
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    coroutineScope.launch {
+                        drawerState.open()
+                    }
+                }
+            ) {
+                Icon(Icons.Filled.Menu, contentDescription = "Menu")
+            }
+        }
+    )
+}
